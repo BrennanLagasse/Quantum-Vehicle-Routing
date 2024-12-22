e9bbe9b6e8d51d37575be6fde625c6f28812e4b3
BrennanLagasse <brennan.lagasse@gmail.com>
2024-12-21 21:30:32 -0500

    Added testing features, started naive binary implemtation (needs new solver)

--- vrp.py
+++ vrp.py
@@ -14,13 +14,12 @@ from dwave.system import LeapHybridCQMSampler
 
 import math
 
-
-
 class QuboForm(Enum):
     CLASSIC = 1
     PATH_EFFICIENT = 2
     PATH_EFFICIENT_BINARY = 3
     EDGE_MATRIX = 4
+
 
 class VehicleRoutingModel(ABC):
     def __init__(self, num_locations: int, distances: list[list[int]], num_vehicles: int, max_distance: int):
@@ -260,6 +259,106 @@ class BoundedPathModel(VehicleRoutingModel):
 
             self.cqm.add_constraint(sum <= self.max_distance,
                                 label=f"Vehicle {m} drives more than the maximum capacity")
-    
-    
 
+# TODO: Rewrite to use an appropriate higher-order solver
+class NaiveBinaryModel(VehicleRoutingModel):
+    
+    def __init__(self, num_locations: int, distances: list[list[int]], num_vehicles: int, max_distance: int):
+        super().__init__(num_locations, distances, num_vehicles, max_distance)
+
+    def get_distance(self, i, j):
+        """Pad the distance array to decentivise selecting invalid distances, avoid array miss"""
+        if i <= self.num_locations and j <= self.num_locations:
+            return self.distance[i][j]
+        else:
+            # Arbitrary high weight so that another route is incentivised
+            return self.max_distance * 2
+
+    def _check_location(self, vehicle, location, time):
+        """Return the binary expression to check if a vehicle is at a location at a time"""
+
+        term = 1
+        indices = [int(bit) for bit in bin(location)[2:]]
+
+        for idx, val in enumerate(indices):
+            if val == 0:
+                term *= (1-self.x[vehicle, idx, time])
+            else:
+                term *= self.x[vehicle, idx, time]
+
+        return term
+
+
+    def _construct_objective(self):
+
+        self.obj = None
+
+        M = self.num_vehicles
+        N = self.num_locations
+        N2 = 2**math.ceil(math.log(N+1, 2))
+
+        # Create all the variables: one for each vehicle/location/position combo
+        # (i=vehicle, j=vertex (one of the indices), k=timestep)
+        self.x = {(i, j, k): Binary(str(i) + "_" + str(j) + "_" + str(k)) for k in range(N) for j in range(math.ceil(math.log(N+1, 2))) for i in range(M)}
+
+        print(self.x.keys())
+
+        # Define the unconstrained binary optimization problem
+        self.obj = BinaryQuadraticModel(vartype="BINARY")
+
+        # The cost of going from the depot to the first stop
+        for m in range(M):
+            for n in range(1, N2):
+                self.obj += self._check_location(m, n, 0) * self.distances[0][n]
+
+        # The cost of going from the last stop to the depot
+        for m in range(M):
+            for n in range(1, N2):
+                self.obj += self._check_location(m, n, N-1) * self.distances[n][0]
+
+        # The cost of going between all stops in the middle
+        for m in range(M):
+            for t in range(N+1):
+                for i in range(N2):
+                    for j in range(N2):
+                        self.obj += self._check_location(m, i, t) * self._check_location(m, j, t+1) * self.distances[i][j]
+
+        self.cqm.set_objective(self.obj)
+    
+    def _add_constraints(self):
+
+        print("Adding constraints")
+
+        M = self.num_vehicles
+        N = self.num_locations
+
+        # 1. Each location should be served by exactly one vehicle (does not check first since depot is required start and end location by construction)
+        for j in range(1, N+1):
+            sum = 0
+            for m in range(M):
+                for t in range(N):
+                    sum += self._check_location(m, j, t)
+
+            # Could be relaxed to geq, the rest should be handled by the objective
+            self.cqm.add_constraint(sum == 1,
+                            label=f"Vertex {j} is not visited or visited more than once")
+
+        # 2. Each vehicle is in one location - unneccessary for this variant
+                
+        #3. Each vehicle drives less than the cap
+        for m in range(M):
+            sum = 0
+
+            for i in range(N+1):
+                
+                # Add in the distances from deport to first, last to depot
+                sum += self._check_location(m, i, 0)*self.get_distance(0, i)
+                sum += self._check_location(m, i, N-1)*self.get_distance(i, 0)
+
+                # Go through the steps in the middle
+                for j in range(N+1):
+                    for t in range(N-1):
+                        sum += self._check_location(m, i, t)*self._check_location(m, j, t+1)*self.get_distance(i,j)
+
+            self.cqm.add_constraint(sum <= self.max_distance,
+                                label=f"Vehicle {m} drives more than the maximum capacity")
--- vrp_scaling.py
+++ vrp_scaling.py
@@ -9,54 +9,74 @@ import pickle
 import random
 random.seed(7)
 
-solvers = {'default': DefaultRoutingModel, 'bounded_path': BoundedPathModel}
+solvers = {'default': DefaultRoutingModel, 'bounded_path': BoundedPathModel, 'naive_binary': NaiveBinaryModel}
 
 # Process the input, get the solver to use
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("solver", type=str, help="Name of QUBO solver to use ('default', 'path_efficient')")
+parser.add_argument("solver", type=str, help="Name of QUBO solver to use ('default', 'bounded_path', 'naive_binary')")
+parser.add_argument("--problem", type=str, default=None, help="Name of file to test on. If not provided, will test on all")
 args = parser.parse_args()
 solver = args.solver
+problem = args.problem
 assert solver in solvers
 
 print(f"Using {solver} solver\n")
 
+if problem:
+    print(f"Custom problem: {problem}")
+else:
+    print("Running on full problem suite")
 
-for m in range(1, 7):
-    for v in range(3):
+def solve_instance(solver: str, problem: str):
+    """Takes a solver and filepath and returns the results from the annealer"""
 
-        instance_str = f'vrp_{m}m_{5*m}n_{60}c_v{v}'
+    print(f"Testing {solver} solver on {problem}")
 
-        print(f"Running {instance_str}")
+    # Unpack the saved problem instance
+    with open(f'routing_problems/{problem}.pkl', 'rb') as file:
+        problem = pickle.load(file)
 
-        # Unpack the saved problem instance
-        with open(f'routing_problems/{instance_str}.pkl', 'rb') as file:
-            problem = pickle.load(file)
+    # Create the solver
+    model = solvers[solver](
+        num_locations = len(problem.cost_matrix) - 1, 
+        distances = problem.cost_matrix, 
+        num_vehicles = problem.num_vehicles, 
+        max_distance = problem.vehicle_capacity
+    )
+    model.build_constrained_model()
 
-        # Create the solver
-        model = solvers[solver](
-            num_locations = len(problem.cost_matrix) - 1, 
-            distances = problem.cost_matrix, 
-            num_vehicles = problem.num_vehicles, 
-            max_distance = problem.vehicle_capacity
-        )
-        model.build_constrained_model()
+    # Run the model and save the 
+    t1 = time.time()
+    sample = model.run_constrained_model(TOKEN)
+    t2 = time.time()
 
-        # Run the model and save the 
-        t1 = time.time()
-        sample = model.run_constrained_model(TOKEN)
-        t2 = time.time()
+    solution = RoutingSolution(
+        num_variables = len(model.x),
+        num_constraints = len(model.cqm.constraints),
+        num_biases = model.cqm.num_biases(),
+        samples = sample,
+        time = t2 - t1
+    )
 
-        solution = RoutingSolution(
-            num_variables = len(model.x),
-            num_constraints = len(model.cqm.constraints),
-            num_biases = model.cqm.num_biases(),
-            samples = sample,
-            time = t2 - t1
-        )
+    with open(f'results/{solver}/{problem}.pkl', 'wb') as out:
+        pickle.dump(solution, out, pickle.HIGHEST_PROTOCOL)
 
-        with open(f'results/{solver}/{instance_str}.pkl', 'wb') as out:
-            pickle.dump(solution, out, pickle.HIGHEST_PROTOCOL)
+
+if problem:
+    # Solve the given problem
+    solve_instance(solver, problem)
+
+else:
+    # Test on all problems in the benchmark
+    for m in range(1, 7):
+        for v in range(3):
+
+            problem = f'vrp_{m}m_{5*m}n_{60}c_v{v}'
+
+            solve_instance(solver, problem)
+
+        
 
 
 
